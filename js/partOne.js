49f5e71e2708bf43702cac7f80cb89934dab326e
Cristina Romero <criromllo@gmail.com>
2024-10-18 17:00:05 +0200

    ejercicio terminado

--- js/partOne.js
+++ js/partOne.js
@@ -1,10 +1,6 @@
 document.querySelector(".button").addEventListener("click", function() {
     document.body.style.backgroundColor = "darkblue";
     document.querySelector(".color").textContent = "darkblue";
+    document.querySelector(".button").textContent = "darkblue";
 });
-document.getElementsByClassName(".button")
-    button.addEventListener("click", function() {
-        button.textContent = "Darkblue!";
-});
-const button = document.getElementById("myButton");
 
